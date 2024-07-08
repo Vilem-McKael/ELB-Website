0f4d452ccbf23be4d051ee1c6995db4c937cd0a0
Vilem-McKael <vilemckael@gmail.com>
2024-07-08 16:40:24 -0700

    changes shadow and some responsive styling for up and coming cards

--- src/components/Home/UpAndComing.jsx
+++ src/components/Home/UpAndComing.jsx
@@ -20,7 +20,7 @@ export default function UpAndComing() {
         
         {/* 1: THE ACOLYTE */}
         <div className='flex flex-col w-full sm:w-[640px] md:w-full lg:w-[900px] xl:w-[1100px] items-center'>
-            <div className='flex flex-col items-center md:flex-row text-wrap w-full sm:w-[640px] md:w-full lg:w-[900px] xl:w-[1100px] md:h-[500px] mb-8 md:shadow-lg md:shadow-black'>
+            <div className='flex flex-col items-center md:flex-row text-wrap w-full sm:w-[640px] md:w-full lg:w-[900px] xl:w-[1100px] md:h-[500px] mb-8 md:shadow-lg md:shadow-gray-500'>
                 
                 <img src={theAcolyte} className='h-full w-full sm:w-[400px] md:w-[400px] object-cover object-top'/>
                 <div className='flex flex-col w-full sm:w-[400px] md:w-full h-full justify-center items-start p-4 sm:px-0 md:py-0 md:px-8 '>
@@ -33,15 +33,15 @@ export default function UpAndComing() {
                     <div className='pt-2 text-md italic md:ml-8'>
                         <span className='font-bold'>IMDB: </span>Star Wars series that takes viewers into a galaxy of shadowy secrets and emerging dark-side powers in the final days of the High Republic era.
                     </div>
-                    <div className='md:ml-8 text-sm pt-4 flex flex-col md:flex-row w-full items-center'>
+                    <div className='md:ml-8 text-sm pt-4 flex flex-col lg:flex-row w-full items-center md:items-start'>
                         Episode I premieres June 4, 2024 only on
-                        <img src={'/studioLogos/disneyPlus.png'} className='w-[100px] pt-2 md:pt-0 md:pl-2'/>
+                        <img src={'/studioLogos/disneyPlus.png'} className='w-[100px] pt-2 lg:pt-0 lg:pl-2'/>
                     </div>
                 </div>
             </div>
 
             {/* 2: KINGDOM OF THE PLANET OF THE APES */}
-            <div className='flex flex-col items-center md:flex-row text-wrap w-full sm:w-[640px] md:w-full lg:w-[900px] xl:w-[1100px] md:h-[500px] mb-8 md:shadow-lg md:shadow-black'>
+            <div className='flex flex-col items-center md:flex-row text-wrap w-full sm:w-[640px] md:w-full lg:w-[900px] xl:w-[1100px] md:h-[500px] mb-8 md:shadow-lg md:shadow-gray-500'>
                 <img src={kingdom} className='h-full w-full sm:w-[400px] md:w-[400px] object-cover object-top'/>
                 <div className='flex flex-col w-full sm:w-[400px] md:w-full h-full justify-center items-start p-4 sm:px-0 md:py-0 md:px-8 object-left-top'>
                     <div className='text-4xl font-semibold'>
@@ -60,7 +60,7 @@ export default function UpAndComing() {
             </div>
 
             {/* 3: THELMA THE UNICORN */}
-            <div className='flex flex-col items-center md:flex-row text-wrap w-full sm:w-[640px] md:w-full lg:w-[900px] xl:w-[1100px] md:h-[500px] mb-8 md:shadow-lg md:shadow-black'>
+            <div className='flex flex-col items-center md:flex-row text-wrap w-full sm:w-[640px] md:w-full lg:w-[900px] xl:w-[1100px] md:h-[500px] mb-8 md:shadow-lg md:shadow-gray-500'>
                 <img src={thelmaTheUnicorn} className='h-full w-full sm:w-[400px] md:w-[400px] object-cover object-top'/>
                 <div className='flex flex-col w-full sm:w-[400px] md:w-full h-full justify-center items-start p-4 sm:px-0 md:py-0 md:px-8 object-left-top'>
                     <div className='text-4xl font-semibold'>
@@ -72,9 +72,9 @@ export default function UpAndComing() {
                     <div className='pt-2 text-md italic md:ml-8'>
                         <span className='font-bold'>IMDB: </span>When a rare pink and glitter-filled moment of fate makes Thelma the Pony's wish of being a unicorn come true, she rises to instant international pop-superstar stardom, but at an unexpected cost.
                     </div>
-                    <div className='w-full md:ml-8 text-sm pt-4 flex flex-col md:flex-row items-center'>
+                    <div className='w-full md:ml-8 text-sm pt-4 flex flex-col lg:flex-row items-center md:items-start'>
                         <div className=''>Released on May 17, 2024, in theaters and on</div>
-                        <img src={'/studioLogos/netflix2.png'} className='w-[100px] pt-2 md:pt-0 md:pl-2'/>
+                        <img src={'/studioLogos/netflix2.png'} className='w-[100px] pt-2 lg:pt-0 md:pl-2'/>
                     </div>
                 </div>
             </div>
